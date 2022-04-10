f66b60b4d986941820403de5901f17bc90f6967b
Simeon Stanic <80579243+SimkeSt@users.noreply.github.com>
2022-04-10 17:59:58 +0200

    Add files via upload
    First ed

---
+++ MIT/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost="localhost"; //hostname
+$dbuser="codeigniter";  //mysql acc/ username
+$dbpass="codeigniter2019";  //mysql scc/ password
+$dbname="89191031"; //mysql database name
+?>
